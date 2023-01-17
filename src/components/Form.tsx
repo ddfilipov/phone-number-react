62df86bac991a04d07e8a5c3588046dc9bf7e40d
ddfilipov <denisfilipov12+3@gmail.com>
2023-01-17 18:57:57 +0100

    form looking better

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,9 +1,25 @@
-import { FC } from "react";
+import { FC, FormEvent } from "react";
 
 export const Form: FC = () => {
-    return <div>
-        <form>
-            
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label htmlFor="name">Name</label>
+                <input id="name" type="text" />
+            </div>
+            <div>
+                <label htmlFor="surname">Surname</label>
+                <input id="surname" type="text" />
+            </div>
+            <div>
+                <label htmlFor="phoneNumber">Phone Number</label>
+                <input id="phoneNumber" type="text" />
+            </div>
+            <button type="submit">Submit</button>
         </form>
-    </div>;
+    );
 };
